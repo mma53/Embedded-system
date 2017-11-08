ad1922301c74537860f9fe8bf873ac4884f99a3f
mboonstra <mp.boonstra@live.nl>
2017-11-08 13:13:23 +0100

    tryToSendData()
    Added tryToSendData(), also added to scheduler.

--- C/main.c
+++ C/main.c
@@ -12,7 +12,6 @@ volatile int extDist = 0;
 volatile int lightInt = 0;
 volatile int currTemp = 0;
 
-
 void checkExtDist() {
   /* Code to see extension distance here */
 }
@@ -25,14 +24,19 @@ void checkLight() {
   /* Code to check current light intensity here */
 }
 
+void tryToSendData() {
+  /* Code to send collected data to central control */
+}
+
 int main() {
   SCH_Init_T1();
   SCH_Start();
   
   // 10ms/tick, 1s = 100.
-  SCH_Add_Task(checkExtDist, 0, 500);
-  SCH_Add_Task(checkTemp, 0, 4000);
-  SCH_Add_Task(checkLight, 0, 3000);
+  SCH_Add_Task(checkExtDist, 0, 500); // Check screen extension every 5 seconds.
+  SCH_Add_Task(checkTemp, 0, 4000); // Check temperature every 40 seconds.
+  SCH_Add_Task(checkLight, 0, 3000); // Check light intensity every 30 seconds.
+  SCH_Add_Task(tryToSendData, 0, 6000); // Try to send data every 60 seconds.
   
   while(1) {
     SCH_Dispatch_Tasks();
