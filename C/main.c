269355f780415e22fde04d4be51bc0df63a2e90a
mboonstra <mp.boonstra@live.nl>
2017-11-08 14:17:20 +0100

    Sending data, modifying globals
    The program now sends the values of the global variables, after modifying them to provide testing values for the Python team.

--- C/main.c
+++ C/main.c
@@ -5,12 +5,13 @@
 #include <util/delay.h>
 #include "AVR_TTC_scheduler.h"
 
-#define HIGH 0x1
-#define LOW  0x0
-
 volatile int extDist = 0;
 volatile int lightInt = 0;
 volatile int currTemp = 0;
+
+void setupSerial() {
+  Serial.begin(19200);
+}
 
 void checkExtDist() {
   /* Code to see extension distance here */
@@ -24,11 +25,44 @@ void checkLight() {
   /* Code to check current light intensity here */
 }
 
+/* TO BE REMOVED!! Changes a given int by +1 or resets it to 0 when it has reached 10k */
+int modifyValue(int value) {
+  if(value > 10000) {
+    value = 0;
+    return value;
+  }
+  value++;
+  return value;
+}
+
+/* TO BE REMOVED!! Modifies values of the global integers to provide testing input for the Python team */
+void modifyValues() {
+  int i;
+  ext = extDist;
+  modifyValue(ext);
+  extDist = ext;
+  light = lightInt;
+  for(i = 0; i < 3; i++) {
+    modifyValue(light);
+  }
+  lightInt = light;
+  temp = currTemp;
+  for(i = 0; i < 2; i++) {
+    modifyValue(temp);
+  }
+  currTemp = temp;
+}
+
 void tryToSendData() {
-  /* Code to send collected data to central control */
+  modifyValues();
+  Serial.write(extDist);
+  Serial.write(lightInt);
+  Serial.write(currTemp);
 }
 
 int main() {
+  setupSerial();
+  
   SCH_Init_T1();
   SCH_Start();
   
