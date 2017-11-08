5e6273a4d54ce7e62226624cc7b6fda6338971ae
Bjurt <a.h.beishuizen@st.hanze.nl>
2017-11-08 14:21:37 +0100

    Kleine gui

--- Python/Test.py
+++ Python/Test.py
@@ -1,3 +1,20 @@
-w = "Word"
+from tkinter import *
 
-print(w)
+window = Tk()
+
+#label = Label(root, text="Tekst")
+#label.pack()
+
+topFrame = Frame(window)
+topFrame.pack(side=TOP)
+
+bottomFrame = Frame(window)
+bottomFrame.pack(side=BOTTOM)
+
+button1 = Button(topFrame, text= "Button", fg="green")
+button2 = Button(topFrame, text= "Button2", fg="red")
+button3 = Button(topFrame, text= "Button3", fg="blue")
+button4 = Button(bottomFrame, text= "Button4", fg="red")
+
+button1.pack
+window.mainloop()
