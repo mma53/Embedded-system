756ebaed41232c111c3fa9fa7dd9df2d0257ac26
PRiesebos <priesebos@live.com>
2017-11-10 14:16:36 +0100

    Almost complete
    Still need to translate the PulseIn function

--- C/Ultrasoonsensor code v2.c
+++ C/Ultrasoonsensor code v2.c
@@ -2,9 +2,16 @@
 #include <avr/interrupt.h>
 #define F_CPU 16E6
 #include <util/delay.h>
-#include "distance.h"
 
- /* Initialize serial connection with given baudrate. */
+#define HIGH 0x1
+#define LOW 0x0
+
+union {
+  int intValue;
+  char charArray[2];
+} intToChar;
+
+/* Initialize serial connection with given baudrate. */
 void initialize_serial(long baudrate) {
   UBRR0H = 0;
   UBRR0L = ((F_CPU / ( 16 * baudrate ))) - 1;
@@ -12,57 +19,58 @@ void initialize_serial(long baudrate) {
   UCSR0B = _BV(TXEN0) | _BV(RXEN0);
   UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
 }
-//  Initialize and read adc
-int ADCsingleREAD(uint8_t adctouse)
-{
-  int ADCval;
 
-  ADMUX = adctouse;        // use #1 ADC
-  ADMUX |= (1 << REFS0);   // use AVcc as the reference
-  ADMUX &= ~(1 << ADLAR);  // clear for 10 bit resolution
-
-  // 128 prescale for 8Mhz
-  ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
-
-  ADCSRA |= (1 << ADEN);    // Enable the ADC
-  ADCSRA |= (1 << ADSC);    // Start the ADC conversion
-
-  while(ADCSRA & (1 << ADSC)); // waits for the ADC to finish
-
-  ADCval = ADCL;
-  ADCval = (ADCH << 8) + ADCval; // ADCH is read so ADC can be updated again
-
-  return ADCval;
+/* Transmit 8-bit integer */
+// wait for an empty transmit buffer
+// UDRE is set when the transmit buffer is empty
+void transmit(uint8_t data) {
+  loop_until_bit_is_set(UCSR0A, UDRE0);
+  UDR0 = data;
 }
 
-void loop()
-{
-  // const int trigPin = 2; // pind2
-  // const int echoPin = 4; // pind4
-  // establish variables for duration of the ping, 
-  // and the distance result in inches and centimeters:
-  long duration, cm;
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  pinMode(trigPin, OUTPUT); //pind2 is output
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2); //__delay__us(2)
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10); //__delay__us(10)
-  digitalWrite(trigPin, LOW);
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
-  // convert the time into a distance
-  cm = microsecondsToCentimeters(duration);
-  Serial.print("distancia ");
-  Serial.print(cm);
-  Serial.print(" cm");
-  Serial.println();
-  delay(100);
+void transmitInt(int data) {
+  int i;
+  intToChar.intValue = data;\
+  for(i = 0; i < 2; i++) {
+    char toSend = intToChar.charArray[1-i];
+    transmit(toSend);
+  }
 }
+
+/* Receive bits until full byte has been set, then return bit */
+char receive(void) {
+  loop_until_bit_is_set(UCSR0A, RXC0);
+  return UDR0;
+}
+
+// void loop()
+// {
+//   // const int trigPin = 2; // pind2
+//   // const int echoPin = 4; // pind4
+//   // establish variables for duration of the ping, 
+//   // and the distance result in inches and centimeters:
+//   long duration, cm;
+//   // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+//   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+//   pinMode(trigPin, OUTPUT); //pind2 is output
+//   digitalWrite(trigPin, LOW);
+//   delayMicroseconds(2); //__delay__us(2)
+//   digitalWrite(trigPin, HIGH);
+//   delayMicroseconds(10); //__delay__us(10)
+//   digitalWrite(trigPin, LOW);
+//   // Read the signal from the sensor: a HIGH pulse whose
+//   // duration is the time (in microseconds) from the sending
+//   // of the ping to the reception of its echo off of an object.
+//   pinMode(echoPin, INPUT);
+//   duration = pulseIn(echoPin, HIGH);
+//   // convert the time into a distance
+//   cm = microsecondsToCentimeters(duration);
+//   Serial.print("distancia ");
+//   Serial.print(cm);
+//   Serial.print(" cm");
+//   Serial.println();
+//   delay(100);
+// }
 
 long microsecondsToCentimeters(long microseconds)
 {
@@ -72,7 +80,7 @@ long microsecondsToCentimeters(long microseconds)
   return microseconds / 29 / 2;
 }
 
-int main (void)
+int main()
 {
   /* Set baudrate. */
   initialize_serial(9600);
@@ -84,22 +92,17 @@ int main (void)
    DDRD |= 2;
     while (1) 
     {
+        // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+        // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
         PORTD |= (3<<0);
         _delay_us(2);
         PORTD &= ~(3<<1);
         _delay_us(10);
         PORTD |= (3<<0);
+        duration = pulseIn(4, HIGH);   //nog vertalen naar atmel
+        cm = microsecondsToCentimeters(duration);
+        Serial.print(cm);
+        Serial.print('\n');
+        _delay_ms(100);
     }
-
-  // DDRD = DDRD | 0b11110111;
-
-  while(1) {
-    distance = ADCsingleREAD(0);
-  }
-
-  cm = microsecondsToCentimeters(duration);
-
-  return 0;
-  __delay__ms(100)
-
 }
