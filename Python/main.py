d1e01c7859ec9a4a2b87e1eda772949a99de8c8b
Bjurt <a.h.beishuizen@st.hanze.nl>
2017-11-10 12:22:33 +0100

    main.py proper reading serial
    Now reading characters

--- Python/main.py
+++ Python/main.py
@@ -4,17 +4,19 @@ import serial
 
 ser = serial.Serial()
 ser.baudrate = 19200
-ser.port = 'COM3'
+ser.port = 'COM4'
 ser.open()
 
 print("connected to: " + ser.portstr)
 count = 1
 
 while True:
-    line = ser.readline
-    print('Length of line ' + count + str(': ') + len(line))
-    print(' 1: ' + int(line[0:2]))
-    print(' 2: ' + int(line[2:4]))
-    print(' 3: ' + int(line[4:6]))
+    line = ser.readline()
+    print('Line ' + str(count) + ':')
+    print('  1: ' + str(line[0:2]))
+    print('   int: ' + int(line[0:2]))
+    print('  2: ' + str(line[2:4]))
+    print('  3: ' + str(line[4:6]))
+    print('    ' + str(line[5]))
 
 ser.close()
